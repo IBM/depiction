24c858fae3793b042b11fbf7836bc9e7c8f5fcaf
Matteo Manica <drugilsberg@gmail.com>
2019-11-12 08:17:09 +0100

    feat: Added core KerasModel.
    Signed-off-by: Matteo Manica <drugilsberg@gmail.com>

---
+++ depiction/models/keras/tests/core_test.py
@@ -0,0 +1,23 @@
+"""Test KerasModel."""
+import unittest
+import numpy as np
+from tensorflow import keras
+
+from ....core import Task, DataType
+from ..core import KerasModel
+
+
+class KerasModelTestCase(unittest.TestCase):
+    """Test KerasModel."""
+
+    def test_prediction(self):
+        model = KerasModel(
+            model=keras.applications.MobileNetV2(),
+            task=Task.CLASSIFICATION,
+            data_type=DataType.IMAGE
+        )
+        image = np.random.randn(1, 224, 224, 3)
+        self.assertEqual(
+            model.predict(image).shape,
+            (1, 1000)
+        )
